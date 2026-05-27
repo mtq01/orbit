94ef1f0caac79423a10973803c151dee74542ec1
MIKE <187060323+mtq01@users.noreply.github.com>
2026-05-26 18:40:30 -0700

    added comments to background.js for future ref

--- public/background.js
+++ public/background.js
@@ -1,3 +1,22 @@
+// listens for when the extension icon is clicked in the toolbar
+// chrome.action = extension toolbar button
+// .onClicked = the event
+// .addListener = when this happens, run this function
+// (tab) => { when the icon is clicked, Chrome automatically passes the current tab as an argument 'tab' contains info about that tab (ID, URL, title, etc.)
 chrome.action.onClicked.addListener((tab) => {
+  // open the side panel for this specific tab
+  // tab.id is the unique ID for the tab the developer is currently on
+  // you pass it in so Chrome knows which tab to open the panel for.
   chrome.sidePanel.open({ tabId: tab.id });
 });
+
+
+
+/* Basically...
+
+   When the extension icon is clicked, find out which tab we're on & open the side panel for that tab.
+
+
+   without this code, clicking the orbit extension does NOTHING. 
+
+*/
