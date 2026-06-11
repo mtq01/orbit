a6418fcb6314a8d63706fd4e73978375de803313
MIKE <187060323+mtq01@users.noreply.github.com>
2026-06-10 21:57:19 -0700

    removed fragments <></> from Header & Footer component. Not required.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,6 @@ import { Info, Globe } from "lucide-react";
 
 const Footer = () => {
   return (
-    <>
       <footer className="flex justify-between border-t py-5 px-4 shrink-0">
         <p className="flex gap-2 items-center">
           <Info />
@@ -10,7 +9,6 @@ const Footer = () => {
         </p>
         <Globe />
       </footer>
-    </>
   );
 };
 
